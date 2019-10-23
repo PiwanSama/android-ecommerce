be6c55c2d6876788ec11231d877274c296bc5e1f
Samalie Piwan <samalie.piwan@tollandhaben.com>
2019-10-23 14:27:10 +0300

    Changed typography and color scheme

--- app/src/main/java/com/horlobyte/ctipharmacy/fragments/ProfileFragment.java
+++ app/src/main/java/com/horlobyte/ctipharmacy/fragments/ProfileFragment.java
@@ -1,6 +1,20 @@
 package com.horlobyte.ctipharmacy.fragments;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import androidx.fragment.app.Fragment;
 
+import com.horlobyte.ctipharmacy.R;
+
 public class ProfileFragment extends Fragment {
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        View view = inflater.inflate(R.layout.product_detail, container, false);
+        return view;
+    }
 }
+
