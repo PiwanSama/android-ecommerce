3a819494cce04bc305aa6809526f44060f27b335
Samalie Piwan <samalie.piwan@tollandhaben.com>
2019-10-23 03:03:53 +0300

    Intial Commit

---
+++ app/src/main/java/com/horlobyte/ctipharmacy/App.java
@@ -0,0 +1,32 @@
+package com.horlobyte.ctipharmacy.ctipharmacy;
+
+import android.app.Application;
+import android.content.Context;
+
+/**
+ * Created by arka on 4/28/16.
+ */
+public class App extends Application {
+
+
+    private final String TAG = this.getClass().getSimpleName();
+    private static Context mContext;
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        mContext = this;
+
+    }
+
+    //Logs out user and redirects to login activity
+    public static void logout() {
+        //FirebaseAuth.getInstance().signOut();
+    }
+
+    public static Context getContext(){
+        return mContext;
+    }
+}
---
+++ app/src/main/java/com/horlobyte/ctipharmacy/activities/MainActivity.java
@@ -0,0 +1,75 @@
+package com.horlobyte.ctipharmacy.activities;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.horlobyte.ctipharmacy.R;
+import com.horlobyte.ctipharmacy.fragments.HomeFragment;
+import com.horlobyte.ctipharmacy.fragments.OrdersFragment;
+import com.horlobyte.ctipharmacy.fragments.ProfileFragment;
+import com.horlobyte.ctipharmacy.utils.BottomNavigationBehavior;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActionBar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        toolbar = getSupportActionBar();
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        loadFragment(new HomeFragment());
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.app_bar_menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        return true;
+    }
+
+    private void loadFragment(Fragment fragment){
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_container, fragment);
+        transaction.commit();
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment;
+            switch (item.getItemId()) {
+                case R.id.home_item:
+                    fragment = new HomeFragment();
+                    loadFragment(fragment);
+                    return true;
+
+                case R.id.orders_item:
+                    fragment = new OrdersFragment();
+                    loadFragment(fragment);
+                    return true;
+
+                case R.id.profile_item:
+                    fragment = new ProfileFragment();
+                    loadFragment(fragment);
+                    return true;
+            }
+            return false;
+        }
+    };
+}
